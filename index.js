4def3bfb5238c89f5ab4f3938d95eb95bd891bd0
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-06-28 22:46:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// node server which will handel socket io connections
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on("connection", socket =>{
+    // If a new user joins, let other users know
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // If someone sends a message, broadcast it to others
+    socket.on("send", message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    // Inform all, if someone leaves the chat
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit("left", users[socket.id]);
+        delete users[socket.id];
+    });
+});
